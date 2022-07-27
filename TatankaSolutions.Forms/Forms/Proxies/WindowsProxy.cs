d5f48cb1ac272c7d1abb9ff27e2e3dfbf8016be8
João Xavier <fastjonh@gmail.com>
2022-07-26 22:33:41 -0300

    tentando criar um redirecionador de proxy

--- TatankaSolutions.Forms/Forms/Proxies/WindowsProxy.cs
+++ TatankaSolutions.Forms/Forms/Proxies/WindowsProxy.cs
@@ -1,25 +1,29 @@
-﻿using Microsoft.Win32;
-using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
 
 namespace TatankaSolutions.Forms.Forms.Proxies
 {
     internal class WindowsProxy
     {
-        [DllImport("wininet.dll")]
-        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
-        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
-        public const int INTERNET_OPTION_REFRESH = 37;
+        static WindowsProxy()
+        {
+            var installScript = @"npm install -g proxy-login-automator";
+
+            Process.Start("CMD.exe", installScript);
+        }
 
         public static void ChangeProxy(ProxySetting proxy)
         {
-            var registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true)!;
-            registry.SetValue("ProxyEnable", 1);
-            registry.SetValue("ProxyServer", proxy.IP);
-            registry.SetValue("ProxyUser", proxy.User);
-            registry.SetValue("ProxyPass", proxy.Password);
+            var proxyAdress = proxy.IP.Split(":");
+            var ip = proxyAdress[0];
+            var port = proxyAdress[1];
 
-            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
-            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+            var login_automator_script = @$"node proxy-login-automator.js ^
+                                            - local_port {8081} ^
+                                            -remote_host {ip} ^
+                                            -remote_port {port} ^
+                                            -usr {proxy.User} - pwd {proxy.Password}";
+
+            Process.Start("CMD.exe", login_automator_script);
         }
     }
 }
--- TatankaSolutions.Forms/Program.cs
+++ TatankaSolutions.Forms/Program.cs
@@ -6,7 +6,13 @@ namespace TatankaSolutions
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleException);
             Application.Run(new MainForm());
+        }
+
+        private static void HandleException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
